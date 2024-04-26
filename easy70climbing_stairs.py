b019f227fe9f1e3636c311c3af4db1057b0dc266
Mikhail Chepkin <mihchepi@gmail.com>
2024-04-26 22:59:43 +0400

    easy70climbing_stairs v3 cheat - Time Limit Exceeded

--- easy70climbing_stairs.py
+++ easy70climbing_stairs.py
@@ -7,14 +7,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
 
         def climb_ways(steps_left):
-            if steps_left <= 0:
-                if steps_left == 0:
-                   return 1
+            if steps_left < 0:
                 return 0
-            ways = 0
-            for steps in 1, 2:
-                ways += climb_ways(steps_left - steps)
-            return ways
+            if steps_left == 0:
+                return 1
+            return climb_ways(steps_left - 1) + climb_ways(steps_left - 2)
 
         return climb_ways(n)
 
